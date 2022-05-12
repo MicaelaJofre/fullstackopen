2fc899622d59ee5f7f36d641c7fc2f9e838863e0
Micaela Analia Jofre <mkjofre@gmail.com>
2022-05-12 19:06:56 -0300

    1.6

---
+++ part1/src/History.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const History = ({ prop, text}) => {
+
+    return (
+        <div>
+            <p>{text}: {prop}</p>
+        </div>
+    )
+}
+
+export { History }
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from './Button';
 import { Content } from './Content';
+import { Display } from './Display ';
 import { Header } from './Header';
 import { Total } from './Total';
+import { History } from "./History";
+
+
 
 
 
 const App = () => {
+
+
+  const [counter, setCounter] = useState(0)
+
 
   const course = {
     name: 'Half Stack application development',
@@ -25,7 +34,13 @@ const App = () => {
       }
     ]
   }
+
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
   
+
   return (
     <div>
       <Header course={course.name} />
@@ -33,6 +48,19 @@ const App = () => {
       <Content parts={course.parts} />
       
       <Total parts={course.parts} />
+      
+      
+      
+      <div>
+        <h2>give feedback</h2>
+        <Button onClick={()=>{setGood(good + 1)}} text='good' />
+        <Button onClick={()=>{setNeutral(neutral + 1)}} text='neutral' />
+        <Button onClick={()=>{setBad(bad + 1)}} text='bad' />
+        <h2>statistics</h2>
+        {<History prop={good} text='good' />}
+        {<History prop={neutral} text='neutral' />}
+        {<History prop={bad} text='bad'/>}
+      </div>
     </div>
   )
 };
