6f179b2f5ed0486ebc48f136b1bf68753ac502b2
Micaela Analia Jofre <mkjofre@gmail.com>
2022-05-24 20:10:34 -0300

    2.4

--- part2/courseinfo/src/container/Course.jsx
+++ part2/courseinfo/src/container/Course.jsx
@@ -9,15 +9,22 @@ const Total = ({ parts }) => {
     return <p>Total of {someMagicHere} exercises</p> 
 }
 
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
     return (
-        <div>
-            <Header name={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} /> 
-        </div>
+        <>
+            {
+                courses.map(course => (
+                    <div key={course.id}>
+                        <Header name={course.name} />
+                        <Content parts={course.parts} />
+                        <Total parts={course.parts} />
+                    </div>
+                ))
 
+            }
+        </>
     )
+    
 }
 
 export { Course }
