0f2be5e9af7a6a2307b481f1c6eeb7fb02c8b234
Micaela Analia Jofre <mkjofre@gmail.com>
2022-05-24 21:48:56 -0300

    2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const handleNewName = (e) => {
+    setNewName(e.target.value)
+  }
+  
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name:
+          <input
+            value={newName}
+            onChange={handleNewName}
+          />
+        </div>
+        <div>
+          <button
+            type="submit"
+            onClick={handleSubmit}>
+            add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {
+          persons.map(person => <p key={person.name}>{ person.name }</p>)
+        }
+      </div>
+    </div>
+  )
+}
+
+export  {App};
