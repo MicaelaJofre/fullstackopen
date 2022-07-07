55293eaef9fe9fbee5bbfd1b167717ea73011cea
Micaela Analia Jofre <mkjofre@gmail.com>
2022-07-07 15:47:47 -0300

    2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,37 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Filter } from './components/Filter'
 import { Persons } from './components/Persons'
 import { PersonsForm } from './components/PersonsForm'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    {
-      name: 'Arto Hellas',
-      phone: '39-44-5329993'
-    },
-    {
-      name: 'Ada Lovelace',
-      phone: '39-44-5323523'
-    }, {
-      name: 'Dan Abramov',
-      phone: '12-43-234345'
-    }, {
-      name: 'Mary Poppendieck',
-      phone: '39-23-6423122'
-    }
-  ]) 
+  const [ notes, setNotes ] = useState() 
 
+  useEffect(() => {
+    
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setNotes(response.data)
+      })
+
+  }, [])
   
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter persons={ persons }/>
+      <Filter persons={ notes }/>
       <form>
         <h2>Add a new</h2>
-        <PersonsForm persons={persons} setPersons={ setPersons }/>
+        <PersonsForm persons={notes} setPersons={ setNotes }/>
       </form>
       <h2>Numbers</h2>
-      <Persons persons={persons}/>
+      <Persons persons={notes}/>
     </div>
   )
 }
